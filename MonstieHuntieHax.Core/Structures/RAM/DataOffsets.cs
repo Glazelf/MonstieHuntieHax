c9a9c72c314dde8c83c2cf76d1b7273798725d76
Glazelf <gideon.steenbergen@gmail.com>
2021-06-28 22:19:26 +0200

    automatic code cleanup

--- MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
+++ MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
@@ -12,11 +12,11 @@
         // General
         public const string PointerZeni = "[[[main+210F0F0]+320]+968]+110";
 
-        
+
 
         /// <summary>
         /// Distances
         /// </summary>
-       
+
     }
 }
--- MonstieHuntieHax.WinForms/Main.cs
+++ MonstieHuntieHax.WinForms/Main.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Threading;
-using System.Windows.Forms;
-using MonstieHuntieHax.Core;
+﻿using MonstieHuntieHax.Core;
 using MonstieHuntieHax.Injection;
 using SysBot.Base;
+using System;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace MonstieHuntieHax.WinForms
 {
