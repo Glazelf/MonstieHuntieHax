904cdf5ac507c3de60ab86d8f63480b4fbe7808d
Glazelf <gideon.steenbergen@gmail.com>
2021-06-29 17:01:35 +0200

    Rider HP Pointer

--- MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
+++ MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
@@ -10,13 +10,12 @@
         /// Demo pointers
         /// </summary>
         // General
+        public const string PointerRiderHP = "[[[[[main+2161A70]+98]+148]+60]+10]";
         public const string PointerZeni = "[heap+40731E00]";
-
-
 
         /// <summary>
         /// Distances
         /// </summary>
-
+        
     }
 }
