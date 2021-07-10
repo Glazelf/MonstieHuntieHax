a9d1497c191ad080b97e34db17585d65c89344d9
Glazelf <gideon.steenbergen@gmail.com>
2021-07-10 22:19:54 +0200

    Update pointers to v1.0.3

--- MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
+++ MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
@@ -10,13 +10,21 @@
         /// Demo pointers
         /// </summary>
         // General
-        public const string PointerRiderHP = "[[[[[main+2161A70]+98]+148]+60]+10]";
-        public const string PointerZeni = "[heap+40731E00]";
-        public const string PointerBottleCaps = "[heap+40732D8E]";
+        public const string PointerZeni = "[heap+00486CD5A4]";
+        public const string PointerBottleCaps = "[heap+00484EC414]";
 
         /// <summary>
         /// Distances
         /// </summary>
-        
+
+        /// <summary>
+        /// Everything below here is outdated and won't work for the latest version
+        /// </summary
+
+        /// <summary>
+        /// Unused
+        /// </summary>
+        // Rider HP, can't be updated like this mid-battle, have not found another good usecase for it yet.
+        // public const string PointerRiderHP = "[[[[[main+2161A70]+98]+148]+60]+10]";
     }
 }
