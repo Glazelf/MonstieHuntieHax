134001c75f7fe4bd0180158c3c261a19bb82ac0f
PriestKos <78356612+priestkos@users.noreply.github.com>
2021-07-11 10:41:02 +0530

    Update DataOffsets.cs

--- MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
+++ MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
@@ -4,27 +4,31 @@
     {
         /// <summary>
         /// Main game pointers
+        ///Main game pointers are currently working upto 1.0.3, theoretically item pointers can be found around the bottlecap pointers
         /// </summary>
 
         /// <summary>
         /// Demo pointers
+        /// no work needed
         /// </summary>
-        // General
-        public const string PointerZeni = "[heap+40732670]";
-        public const string PointerBottleCaps = "[heap+407335FE]";
+        
+        // Pointer list
+        public const string PointerZeni = "[heap+40732670]"; //1.0.3 zenny, u32 datatype
+        public const string PointerBottleCaps = "[heap+407335FE]"; //1.0.3 bottle caps, u16 datatype
 
         /// <summary>
         /// Distances
         /// </summary>
 
         /// <summary>
-        /// Everything below here is outdated and won't work for the latest version
+        /// Everything below here is deprecated
         /// </summary
 
         /// <summary>
         /// Unused
         /// </summary>
+       
         // Rider HP, can't be updated like this mid-battle, have not found another good usecase for it yet.
-        // public const string PointerRiderHP = "[[[[[main+2161A70]+98]+148]+60]+10]";
+        // public const string PointerRiderHP = "[[[[[main+2161A70]+98]+148]+60]+10]"; ///need to change to an address in heap.
     }
 }
