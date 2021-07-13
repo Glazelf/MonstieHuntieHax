ad0b42fb12898fff8099f120fce00fe53ad7e463
Glazelf <gideon.steenbergen@gmail.com>
2021-07-13 23:32:18 +0200

    add pointers for charms
    might need to add more pointers or add a distance between them

--- MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
+++ MonstieHuntieHax.Core/Structures/RAM/DataOffsets.cs
@@ -6,7 +6,9 @@
         /// Main game pointers for game version 1.0.3
         /// </summary>
         public const string PointerZeni = "[heap+40732670]"; // u32
-        public const string PointerBottleCaps = "[heap+407335FE]"; //u16
+        public const string PointerBottleCaps = "[heap+407335FE]"; // u16
+        public const string PointerCharmMight = "[heap+40733446]"; // u16
+        public const string PointerCharmMounting = "[heap+40733426]"; // u16
 
         /// <summary>
         /// Distances
